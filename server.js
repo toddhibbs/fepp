ad39ce143a5f8c781973b0059f538250d8438584
Todd Hibbs <todd@toddhibbs.com>
2013-06-12 22:05:34 -0700

    Added express server

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express'),
+    routes = require('./routes'),
+    engines = require('consolidate');
+
+exports.startServer = function(config, callback) {
+  var app = express();
+  var server = app.listen(config.server.port, function() {
+    console.log("Express server listening on port %d in %s mode", server.address().port, app.settings.env);
+  });
+
+  app.configure(function() {
+    app.set('port', config.server.port);
+    app.set('views', config.server.views.path);
+    app.engine(config.server.views.extension, engines[config.server.views.compileWith]);
+    app.set('view engine', config.server.views.extension);
+    app.use(express.favicon());
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.compress());
+    app.use(config.server.base, app.router);
+    app.use(express.static(config.watch.compiledDir));
+  });
+
+  app.configure('development', function() {
+    app.use(express.errorHandler());
+  });
+
+  app.get('/', routes.index(config));
+
+  callback(server);
+};
+
